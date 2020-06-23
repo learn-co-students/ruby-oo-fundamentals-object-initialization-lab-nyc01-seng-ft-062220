4b4c0cbacb093043c4f02fcf3c8c87bb8aefe345
vakas-786 <vakas0959@gmail.com>
2020-06-23 12:14:17 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+    def initialize(name_dog, breed_dog="Mutt")
+        @name = name_dog
+        @breed = breed_dog 
+    end 
+    
+    def name
+        @name
+    end 
+    def breed
+        @breed
+    end 
+end
+
+
